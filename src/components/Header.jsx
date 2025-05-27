6da855c7133684159b2ad38f2316f49142e472e0
Bishar <bishar995@gmail.com>
2025-05-27 09:17:22 +0300

    first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+
 function Header() {
+  const [isDark, setIsDark] = useState(false);
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("darkMode");
+    },
+    [isDark]
+  );
   return (
     <header className="app-header">
       <img src="punt.jpg" alt="Puntland logo" />
       <h1>Puntland Quiz</h1>
+      <button
+        onClick={() => setIsDark((isdark) => !isdark)}
+        className="btn_dark"
+      >
+        {isDark ? "🌙" : "☀️"}
+      </button>
     </header>
   );
 }
